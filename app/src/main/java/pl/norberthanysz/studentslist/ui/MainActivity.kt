bfaaeaab3d11419c677b736b39191e59cdb66f10
Norbert Hanysz <norberthanysz@gmail.com>
2020-08-28 20:52:05 +0200

    Add project configuration and empty activity

---
+++ app/src/main/java/pl/norberthanysz/studentslist/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.norberthanysz.studentslist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.norberthanysz.studentslist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
