e6ea99f8d085783772ca143542ca15594c78e5b2
Norbert Hanysz <norberthanysz@gmail.com>
2020-08-29 16:42:23 +0200

    Close app after double click back button

--- app/src/main/java/pl/norberthanysz/studentslist/ui/MainActivity.kt
+++ app/src/main/java/pl/norberthanysz/studentslist/ui/MainActivity.kt
@@ -2,15 +2,29 @@ package pl.norberthanysz.studentslist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import android.os.Handler
+import android.widget.Toast
 import pl.norberthanysz.studentslist.R
-import pl.norberthanysz.studentslist.repository.ApiAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private var wasBackButtonClicked = false
+    private val backButtonDelay: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        if (wasBackButtonClicked) {
+            super.finish()
+            return
+        }
+
+        this.wasBackButtonClicked = true
+        Toast.makeText(this, getString(R.string.close_app_info_text), Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({ wasBackButtonClicked = false }, backButtonDelay)
     }
 }
